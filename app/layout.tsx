2978a3857de98a75d04d1ef0e5fdaa397470303b
“TariQMah” <t.ali1993@hotmail.com>
2023-04-10 10:45:48 +0500

    setup signin using email password

---
+++ app/layout.tsx
@@ -0,0 +1,43 @@
+import { Nunito } from "next/font/google"
+import getCurentUser from "./actions/getCurrentUser"
+import ClientOnly from "./components/ClientOnly"
+import LoginModal from "./components/modals/LoginModal"
+import RegisterModal from "./components/modals/LoginModal"
+import Navbar from "./components/navbar/Navbar"
+
+import './globals.css'
+import ToasterProvider from "./providers/ToasterProvider"
+
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'airbnb',
+}
+const font = Nunito({
+  subsets: ['latin']
+})
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+
+  const currentUser = await getCurentUser()
+
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <LoginModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <h1 className="text-rose-500 text-2xl">Hello</h1>
+  )
+}
