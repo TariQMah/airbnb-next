3281e63a3ab69c42c56aed081128dedacb8192ac
TariQMah <t.ali1993@hotmail.com>
2023-04-12 05:25:29 +0500

    complete cloudniary image upload

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,6 +7,7 @@ import Heading from "../Heading";
 import CategoryInput from "../inputs/CategoryInput";
 import Counter from "../inputs/Counter";
 import CountrySelect from "../inputs/CountrySelect";
+import ImageUpload from "../inputs/ImageUpload";
 
 import { categories } from "../navbar/Categories";
 import Modal from "./Modal";
@@ -51,6 +52,7 @@ const RentModal = () => {
     const guestCount = watch("guestCount");
     const roomCount = watch("roomCount");
     const bathroomCount = watch("bathroomCount");
+    const imageSrc = watch("imageSrc");
 
     const Map = useMemo(
         () =>
@@ -184,6 +186,26 @@ const RentModal = () => {
                     title="Bathrooms"
                     value={bathroomCount}
                 />
+            </div>
+        );
+    }
+    if (steps === STEPS.IMAGES) {
+        bodyContent = (
+            <div
+                className="
+            flex
+            flex-col
+            gap-8
+            "
+            >
+                <Heading
+                    title="Add a photo of your place"
+                    subtitle="show guests what your place looks like"
+                />
+
+                <ImageUpload onChange={(value) => setCustomValue("imageSrc", value)} value={imageSrc} />
+
+
             </div>
         );
     }
